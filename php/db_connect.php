eaed9c7e919abed9d00d8da24395fec61bcd50dc
Folusho Morafa <folusho.morafa@gmail.com>
2025-10-26 18:06:23 -0500

    connecting database to webspace

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,11 +1,14 @@
 <?php
-$host = "your_host_here";
-$user = "your_username_here";
-$pass = "your_password_here";
-$dbname = "your_db_name_here";
+  $host_name = 'db5018884632.hosting-data.io';
+  $database = 'dbs14898871';
+  $user_name = 'dbu5401157';
+  $password = 'RealT@lker300!!';
 
-$conn = new mysqli($host, $user, $pass, $dbname);
-if ($conn->connect_error) {
-  die("Database connection failed: " . $conn->connect_error);
-}
+  $link = new mysqli($host_name, $user_name, $password, $database);
+
+  if ($link->connect_error) {
+    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+  } else {
+    echo '<p>Connection to MySQL server successfully established.</p>';
+  }
 ?>

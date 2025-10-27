894c3e37f1e2bde4837f1adc1bdbaa67417c30ff
Folusho Morafa <folusho.morafa@gmail.com>
2025-10-26 19:41:36 -0500

    at some point, I changed the variable from conn to link for db_connect, this was causing the variable mismatch

--- php/db_connect.php
+++ php/db_connect.php
@@ -4,10 +4,10 @@
   $user_name = 'dbu5401157';
   $password = 'RealT@lker300!!';
 
-  $link = new mysqli($host_name, $user_name, $password, $database);
+  $conn = new mysqli($host_name, $user_name, $password, $database);
 
-  if ($link->connect_error) {
-    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+  if ($conn->connect_error) {
+    die('<p>Failed to connect to MySQL: '. $conn->connect_error .'</p>');
   } else {
     //echo '<p>Connection to MySQL server successfully established.</p>';
   }
